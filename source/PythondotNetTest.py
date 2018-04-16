a88107b9e14a8ca058f83d9d5e0dd56c65da8636
Jaime Fernandez <fernandezhoffiz@gmail.com>
2018-04-16 15:32:04 -0400

    Updating PythonTest to include both RFSA/G imports

--- source/PythondotNetTest.py
+++ source/PythondotNetTest.py
@@ -14,7 +14,18 @@ sys.path.append(assy_path)
 clr.AddReference("NationalInstruments.ModularInstruments.NIRfsa.Fx40")
 clr.AddReference("NationalInstruments.Common")
 
+dotNetFWDirectory = r"C:\Program Files (x86)\IVI Foundation\IVI\Microsoft.NET\Framework32"
+dotNetClassLibrary = r'v4.0.30319\NationalInstruments.ModularInstruments.NIRfsg 17.1.0'
+assy_path = os.path.join(dotNetFWDirectory, dotNetClassLibrary)
+print(".NET Library: " + dotNetClassLibrary)
+
+sys.path.append(assy_path)
+
+clr.AddReference("NationalInstruments.ModularInstruments.NIRfsg.Fx40")
+clr.AddReference("NationalInstruments.Common")
+
 # Import .NET drivers
 from NationalInstruments.ModularInstruments.NIRfsa import *
+from NationalInstruments.ModularInstruments.NIRfsg import *
 from NationalInstruments import PrecisionTimeSpan
 from NationalInstruments import ComplexDouble
